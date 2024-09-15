947a856a8d55c3d6b5bdc10ed5d5c7827ee854c8
hsmith <hsmith@blacktriangles.com>
2024-09-15 12:09:59 -0500

    adding scrollback functionality

--- config.h
+++ config.h
@@ -210,10 +210,8 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	{ ShiftMask,            Button4, kscrollup,      {.i = 1},      0 },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = 1},      0 },
 };
 
 /* Internal keyboard shortcuts. */
@@ -234,6 +232,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+    { XK_NO_MOD,            XK_Prior,       kscrollup,      {.i = -1} },
+    { XK_NO_MOD,            XK_Next,        kscrolldown,    {.i = -1} },
 };
 
 /*
